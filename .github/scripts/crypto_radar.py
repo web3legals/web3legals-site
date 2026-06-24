1c732c40e8c184710300bc31c2967133c446ede2
web3legals <web3legalsofficial@gmail.com>
2026-06-24 20:19:41 +0530

    Update stylesheet link to absolute path

--- .github/scripts/crypto_radar.py
+++ .github/scripts/crypto_radar.py
@@ -212,7 +212,7 @@ def build_article_html(article, slug):
 <meta name="description" content="{description[:160]}">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 
